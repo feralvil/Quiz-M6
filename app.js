250ed2c5abd9576fbfc5b7f67e7b1c757f3aeb10
Fernando Alfonso <feralvil@gmail.com>
2015-07-14 18:27:10 +0200

    Marco de aplicación

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+// Añadimos el MW express-partials:
+var partials = require('express-partials');
+
 var routes = require('./routes/index');
 
 var app = express();
@@ -12,6 +15,9 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+// Instalamos express-partials:
+app.use(partials());
 
 // uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
