8fb9b2bf1ff0485a9865ff2e6dd7e38cb06ff88b
Fernando Alfonso <feralvil@gmail.com>
2015-07-15 18:38:29 +0200

    Desarrollo de  Creditos

--- routes/index.js
+++ routes/index.js
@@ -14,4 +14,10 @@ router.get('/quizes/question', quizController.question);
 /* GET quizes/answer */
 router.get('/quizes/answer', quizController.answer);
 
+/* GET author page. */
+router.get('/author', function(req, res, next) {
+  res.render('author');
+});
+
+
 module.exports = router;
