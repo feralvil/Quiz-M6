984b72f13c649435c2b335267a4171bda03a31a3
Fernando Alfonso <feralvil@gmail.com>
2015-07-28 10:26:00 +0200

    Agregado buscador de preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -17,11 +17,27 @@ exports.load = function (req, res, next, quizId){
 
 // GET /quizes
 exports.index = function (req,res){
-  models.Quiz.findAll().then(
-    function (quizes){
-      res.render('quizes/index', {quizes: quizes});
-    }
-  ).catch (function(error){next(error);});
+  var textoresp = "No se han encontrado preguntas"
+  if (req.query.search){
+    var search = '%' + req.query.search.replace(' ', '%') + '%';
+    models.Quiz.findAll({where: ["pregunta like ?", search]}).then(
+      function(quizes){
+        if (quizes){
+          textoresp = 'Se han encontrado ' + quizes.length + ' preguntas con el término ' + req.query.search;
+        }
+        res.render('quizes/index', {quizes: quizes, textoresp: textoresp})
+      }
+    ).catch (function(error){next(error);});
+  }
+  else{
+    models.Quiz.findAll().then(
+      function (quizes){
+        var textoresp = 'Se han encontrado ' + quizes.length + ' preguntas';
+        res.render('quizes/index', {quizes: quizes, textoresp: textoresp});
+      }
+    ).catch (function(error){next(error);});
+  }
+
 };
 
 // GET /quizes/:id
